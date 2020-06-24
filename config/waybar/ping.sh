2b839f15ec0e61cb69eaada4d3f6426b8171d04b
Nenad Stojanovikj <me+github@nenad.dev>
2020-06-24 22:12:20 +0200

    Update sway and waybar configuration

---
+++ config/waybar/ping.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+tmpfile=/tmp/waybar-ping
+if [ -f $tmpfile ]; then
+	display="$(printf '%15s' $(cat $tmpfile))"
+else
+	display="$(printf '%15s' '')"
+fi
+
+result=$(ping -4 -c 1 google.com -w 2)
+success=$(echo "${result}" | grep received | cut -d "," -f2 | sed 's/received//' | tr --delete " ")
+time=$(echo "${result}" | grep ttl | cut -d " " -f8 | sed 's/time=//')
+
+if [ $success = "1" ]; then
+	if [ $(echo $time'<40' | bc -l) -eq 1 ]; then
+		
+		display="${display:1}▁"
+	elif [ $(echo $time'<80' | bc -l) -eq 1 ]; then
+		display="${display:1}‗"
+	elif [ $(echo $time'<150' | bc -l) -eq 1 ]; then
+		display="${display:1}⎻"
+	elif [ $(echo $time'<300' | bc -l) -eq 1 ]; then
+		display="${display:1}▔"
+	else
+		display="${display:1}~"
+	fi
+else
+	display="${display:1}X"
+fi
+
+echo "${time}ms ${display}" 
+echo "$display" > $tmpfile
