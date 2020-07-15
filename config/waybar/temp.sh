a5677706e53e8581b4d14f51661c8db16b9841bd
Nenad Stojanovikj <me+github@nenad.dev>
2020-07-15 18:05:37 +0200

    [waybar] Add degree symbol on custom/temp

--- config/waybar/temp.sh
+++ config/waybar/temp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sensors -u | grep Core -A 1 | grep temp | cut -d ":" -f 2 | awk 'BEGIN { sum=0; n=0; } { sum+=$1; n++; } END { print sum/n }' | xargs printf '%.2f &#xf2c9;'
+sensors -u | grep Core -A 1 | grep temp | cut -d ":" -f 2 | awk 'BEGIN { sum=0; n=0; } { sum+=$1; n++; } END { print sum/n }' | xargs printf '%.1f&#xb0;C &#xf2c9;'
