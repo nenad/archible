b68f41caefab03cd582c65160033412b8dda94a0
Nenad Stojanovikj <me+github@nenad.dev>
2021-11-09 22:24:07 +0100

    [init] Move cowspace check to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-set -ex
+set -eo pipefail
+
+bytes_free=$(df /run/archiso/cowspace --output=size | tail -1)
+if [ "$bytes_free" -lt 1048576 ]; then
+	echo "The /run/archiso/cowspace disk space is less than 1 GB. Restart with kernel parameter 'cow_spacesize=1G'"
+	exit 1
+fi
 
 pacman -S --needed ansible
+
 ansible-playbook -i 127.0.0.1, -K init-playbook.yml
